ac0b386f3ed0467465786f804665bb781a72a5ee
zoyeras <loaizaocampos@gmail.com>
2026-03-01 00:05:58 -0500

    feat(db): add dbcontext

--- TaskManager_backend/Data/AppDbContext.cs
+++ TaskManager_backend/Data/AppDbContext.cs
@@ -1,6 +1,12 @@
+using TaskManager_backend.Models;
+
 namespace TaskManager_backend.Data;
 
-public class AppDbContext
+public class AppDbContext:DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<User> Users { get; set; }
+    public DbSet<TaskItem> Tasks { get; set; }
 }
--- TaskManager_backend/Program.cs
+++ TaskManager_backend/Program.cs
@@ -1,4 +1,8 @@
+using TaskManager_backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
